8af05aeff9dce0f66ceac2568cd88c91f7e5bc6a
Parlinggoman <89424392+Parlinggoman@users.noreply.github.com>
2021-08-24 18:48:27 +0700

    Create PemilikHewan.cs

---
+++ PemilikHewan.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AplikasiConsoleOop
+{
+    class PemilikHewan : Hewan
+    {
+       // public string idPemilikHewan { get; set; }
+//public string namaPemilik { get; set; }
+        public string noHp { get; set; }
+        public string alamat { get;  set; }
+
+        public void KepemilikanHewan()
+        {
+            Console.WriteLine("===== Data Pemilik Hewan =====");
+        }
+       // public void Id()
+       // {
+       //     Console.WriteLine($"ID Pemilik Hewan : {this.idPemilikHewan}");
+      //  }
+      //  public void NamaPemilik()
+      //  {
+        //    Console.WriteLine($"Nama Pemilik Hewan : {this.namaPemilik}");
+      //  }
+        public void NoHP()
+        {
+            Console.WriteLine($"No Hanphone Pemilik Hewan: {this.noHp}");
+        }
+        public void Alamat()
+        { 
+            Console.WriteLine(  $"Alamat Pemilik Hewan : {this.alamat}");
+        }
+
+        public void DataPemilik( )
+        {
+            Console.WriteLine("===== DATA PEMILIK HEWAN =====");
+            Console.WriteLine($"ID Pemilik Hewan: {id}");
+            Console.WriteLine($"Nama Pemilik Hewan : {nama}");
+                Console.WriteLine(  $"Tinggi Pemilik :{tinggi}");
+            Console.WriteLine($"No Hanphone Pemilik Hewan: {noHp}");
+            Console.WriteLine($"Alamat Pemilik Hewan : {alamat}");
+            Console.WriteLine($"Jumlah Kaki : {jumlahKaki}");
+            Console.WriteLine($"Jumlah Pasang Kaki :{BerapaJumlahPasangKaki()}");
+            Console.WriteLine($"ID Hewan : {id}");
+            Console.WriteLine($"Riwayat Penyakit Peliharaan :  {riwayatPenyakit}");
+        }
+
+    }
+   
+}
